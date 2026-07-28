3ecafe8e65d08dd2005e9275043820c5d1f31c8b
GWC1111 <wonil4198@gmail.com>
2026-07-28 18:12:15 +0900

    mypage update

--- src/main/java/net/likelion/bebc25/recipe/member/memberController.java
+++ src/main/java/net/likelion/bebc25/recipe/member/memberController.java
@@ -1,4 +1,31 @@
 package net.likelion.bebc25.recipe.member;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
 public class memberController {
+
+    @GetMapping("/find-account")
+    public String findAccount() {
+        return "member/find-account.html";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "member/login.html";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "member/register.html";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage() {
+        return "member/mypage.html";
+    }
 }
