f4c1660ed9d0b95637e631997ff3fd7909738320
LEEJIWON <caelumean215@gmail.com>
2026-07-28 17:48:41 +0900

    레시피리스트,레시피등ë록,팁리스트,팁 등록,오늘 ㅁ뭐먹지 html,css,controller

--- src/main/java/net/likelion/bebc25/recipe/post/RecipePostController.java
+++ src/main/java/net/likelion/bebc25/recipe/post/RecipePostController.java
@@ -21,5 +21,29 @@ public class RecipePostController {
         return "redirect:/board/recipe-details.html";
     }
 
+    // recipe-write 화면 보여주는 컨트롤러
+    @GetMapping("/recipe-write.html")
+    public String getRecipeWriteList(){
+        return "board/recipe-write";
+
+    }
+
+    // todayMenu 화면 보여주는 컨트롤러
+    @GetMapping("todayMenu.html")
+    public String getTodayMenu(){
+        return "board/todayMenu";
+    }
+
+    // 요리 꿀팁 리스트 보여주는 컨트롤러
+    @GetMapping("tip-list.html")
+    public String getTipList(){
+        return "board/tip-list";
+    }
+
+    // 요리 꿀팁 작성하는 화면을 보여주는 컨트롤러
+    @GetMapping("tip-write.html")
+    public String getTipWrite(){
+        return "board/tip-write";
+    }
 
 }
