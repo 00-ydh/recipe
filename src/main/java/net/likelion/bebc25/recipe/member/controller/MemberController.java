eb7e3e7fa1baae39fb680a21a5e7c6f6a5370d86
GWC1111 <wonil4198@gmail.com>
2026-07-29 14:42:44 +0900

    member update

---
+++ src/main/java/net/likelion/bebc25/recipe/member/controller/MemberController.java
@@ -0,0 +1,116 @@
+package net.likelion.bebc25.recipe.member.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import net.likelion.bebc25.recipe.member.dto.MemberDto;
+import net.likelion.bebc25.recipe.member.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Slf4j
+@RequestMapping("/member")
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    /**
+     * 비밀번호 찾기 양식 화면을 반환합니다.
+     * @param memberDto 폼과 바인딩할 빈 회원 객체
+     * @return 비밀번호 찾기 페이지 뷰 경로
+     */
+    @GetMapping("/find-account")
+    public String getFindAccountForm(@ModelAttribute("member") MemberDto memberDto) {
+        return "member/find-account";
+    }
+
+    /**
+     * 회원의 이메일, 별명이 일치하면 비밀번호를 보여줍니다.
+     * @param memberDto 비밀번호를 찾을 회원 정보 객체
+     * @param model 결과 메시지를 전달하기 위한 model 객체
+     * @return 비밀번호 찾기 결과가 포함된 뷰 경로
+     */
+    @PostMapping("/find-account")
+    public String findAccount(@ModelAttribute("member") MemberDto memberDto, Model model) {
+        String resultPassword = memberService.findPassword(memberDto.getEmail(), memberDto.getName());
+
+        model.addAttribute("resultMessage", resultPassword);
+        return "member/find-account";
+    }
+
+    /**
+     * 로그인 양식 화면을 반환합니다.
+     * @return 로그인 페이지 뷰 경로
+     */
+    @GetMapping("/login")
+    public String getLoginForm() {
+        return "member/login";
+    }
+
+    /**
+     * 이메일과 비밀번호를 입력받아 검증 성공 시 메인 화면으로 이동합니다.
+     * @param email 입력 받은 이메일
+     * @param password 입력 받은 비밀번호
+     * @return 성공 시 메인 화면, 실패 시 로그인 페이지
+     */
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password) {
+        if (memberService.login(email, password) != null) {
+            return "redirect:/index.html";
+        } else {
+            return "member/login";
+        }
+    }
+
+    /**
+     * 회원 가입 양식 화면을 반환합니다.
+     * @param memberDto 폼과 바인딩할 빈 회원 정보 객체
+     * @return 회원가입 페이지 뷰 경로
+     */
+    @GetMapping("/register")
+    public String getRegisterForm(@ModelAttribute("member") MemberDto memberDto) {
+        return "member/register.html";
+    }
+
+    /**
+     * 회원가입을 처리합니다. 중복된 계정이 있을 경우 가입을 허용하지 않습니다.
+     * 입력값 검증 실패 시 가입 폼으로 돌아갑니다.
+     * @param memberDto 회원 가입 할 회원 정보 DTO
+     * @param bindingResult 검증 오류 결과 객체
+     * @param model 화면에 에러 메시지를 전달하기 위한 Model 객체
+     * @return 검증 실패/중복 시 가입 폼 경로, 가입 성공 시 로그인 페이지로 리다이렉트
+     */
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("member") MemberDto memberDto, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()){
+            return "member/register";
+        }
+
+        if(!(memberService.register(memberDto))) {
+            model.addAttribute("errorMessage", "중복된 이메일입니다.");
+            return "member/register";
+        }
+
+        return "redirect:/member/login";
+    }
+
+    @GetMapping("/mypage")
+    public String myPage() {
+        return "member/mypage.html";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "member/profile.html";
+    }
+
+    @GetMapping("/edit")
+    public String userEdit() {
+        return "member/user-edit.html";
+    }
+}
---
+++ src/main/java/net/likelion/bebc25/recipe/member/repository/MemberRepository.java
@@ -0,0 +1,45 @@
+package net.likelion.bebc25.recipe.member.repository;
+
+import net.likelion.bebc25.recipe.member.dto.MemberDto;
+
+import java.util.List;
+
+public interface MemberRepository {
+    /**
+     * 새로운 회원 정보를 저장합니다.
+     * @param memberDto 저장할 회원 정보 DTO
+     */
+    void save(MemberDto memberDto);
+
+    /**
+     * 회원 고유 식별자를 기반으로 회원 정보를 조회합니다.
+     * @param id 조회할 회원 고유 식별자
+     * @return 조회된 회원 정보 DTO, 없을 경우 null 반환
+     */
+    MemberDto findById(int id);
+
+    /**
+     * 기존 회원 정보를 수정합니다.
+     * @param memberDto 수정할 회원 정보 DTO
+     */
+    void update(MemberDto memberDto);
+
+    /**
+     * 회원 고유 식별자를 기반으로 회원 정보를 삭제합니다.
+     * @param id 삭제할 회원 고유 식별자
+     */
+    void deleteById(int id);
+
+    /**
+     * 저장소에 있는 모든 회원 정보를 조회합니다.
+     * @return 전체 회원 목록
+     */
+    List<MemberDto> findAll();
+
+    /**
+     * 회원 이메일을 기반으로 회원 정보를 조회합니다.
+     * @param email 조회할 로그인 이메일
+     * @return 조회된 회원 정보 DTO, 없을 경우 null 반환
+     */
+    MemberDto findByEmail(String email);
+}
