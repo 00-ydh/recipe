7dd11dd263326adf9c9540d4afba336be725f64e
GWC1111 <wonil4198@gmail.com>
2026-08-03 13:39:48 +0900

    feat: interceptor 추가

---
+++ src/main/java/net/likelion/bebc25/recipe/config/WebConfig.java
@@ -0,0 +1,17 @@
+package net.likelion.bebc25.recipe.config;
+
+import net.likelion.bebc25.recipe.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/member/**")
+                .excludePathPatterns("/member/login", "/member/register", "/member/profile/**", "/member/css/**", "/js/**", "/images/**", "/*.ico", "/error");
+    }
+}
